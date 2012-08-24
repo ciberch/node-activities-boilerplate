78d43c213af55b2e79b3530818419761fd1fefb2
ciberch <monica.keller@gmail.com>
2012-08-23 18:35:14 -0700

    Defaults for Backbone Models - still not sure if I like having to define for a second time my schema client side

--- public/js/backbone/models.js
+++ public/js/backbone/models.js
@@ -1,13 +1,50 @@
-var Activity = Backbone.Model.extend({});
+var Activity = Backbone.Model.extend({
+
+    // From activity-streams-mongoose/lib/activityMongoose.js
+    defaults: {
+        verb: 'post',
+        object: null, //ActivityObject
+        actor: null, //ActivityObject
+        url: '',
+        title: '',
+        content: '',
+        icon: null, // MediaLinkHash
+        target: null, //ActivityObject
+        published: Date.now,
+        updated: Date.now,
+        inReplyTo: null, //Activity
+        provider: null, //ActivityObject
+        generator: null, //ActivityObject
+        streams: ['firehose']
+    }
+});
 var ActivityList = Backbone.Collection.extend({
 	model: Activity,
     url : "/activities"
 });
 
-var ActivityObject = Backbone.Model.extend({});
+var ActivityObject = Backbone.Model.extend({
+    defaults : {
+        image: null, // MediaLinkHash
+        icon: null, // MediaLinkHash
+        displayName: '',
+        summary: '',
+        content: '',
+        url: '',
+        author: null, //ActivityObject
+        published: Date.now,
+        objectType: '',
+        attachments: [], //ActivityObject
+        upstreamDuplicates: [], //string
+        downstreamDuplicates: [], //string
+        updated: Date.now
+    }
+
+});
 
 var ActivityObjectType = Backbone.Model.extend({});
-var ActivityObjectList = Backbone.Collection.extend({
+
+var ActivityObjectTypeList = Backbone.Collection.extend({
 	model : ActivityObjectType,
 	url : "/objectTypes"
 });
